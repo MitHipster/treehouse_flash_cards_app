7bfd03ac42bd6bfd4071616a9a9a53de435cd93f
Tim Acker <tjacker31@gmail.com>
2018-09-01 14:53:10 -0400

    Handlebars
    Added express handlebars as templating engine for project.

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }));
+app.set('view engine', 'hbs');
+
 app.get('/', (req, res) => {
-	res.send('Hi Oh!');
+	res.render('home');
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+	console.log('Application running on localhost:3000');
+});
