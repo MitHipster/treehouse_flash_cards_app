fa3f0b2ae9e0880439c65d75f1262038f0a3cad2
Tim Acker <tjacker31@gmail.com>
2018-09-01 22:47:01 -0400

    Username
    Added input for student to enter their name for custom welcome message.

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
 	res.render('index');
+});
+
+app.get('/hello', (req, res) => {
+	res.render('hello');
+});
+
+app.post('/hello', (req, res) => {
+	res.render('hello', { name: req.body.username });
 });
 
 app.get('/cards', (req, res) => {
