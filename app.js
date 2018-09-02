c9833c36a9497f741f845dec9c4705ba28fe544f
Tim Acker <tjacker31@gmail.com>
2018-09-02 19:17:23 -0400

    Error Handler
    Customized the express error handler in server.js

--- app.js
+++ app.js
@@ -46,6 +46,18 @@ app.get('/cards', (req, res) => {
 	});
 });
 
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+app.use((err, req, res, next) => {
+	res.locals.error = err;
+	res.status(err.status);
+	res.render('error');
+});
+
 app.listen(3000, () => {
 	console.log('Application running on localhost:3000');
 });
