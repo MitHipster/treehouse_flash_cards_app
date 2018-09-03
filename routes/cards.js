26aae2bd637f041d81207003b8a69803185e77bc
Tim Acker <tjacker31@gmail.com>
2018-09-02 22:03:48 -0400

    Flashcards
    Added a JSON file to hold flashcard data and modified cards route to display question and answers using URL query strings.

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const { data } = require('../data/flashcard.json');
+const { cards } = data;
 
 // Since the 'cards' root is defined in the use method in app.js, it is removed from the route below
-router.get('/', (req, res) => {
-	res.render('index', {
-		prompt: "Who is buried in Grant's tomb?",
-		hint: 'Think about whose tomb it is.'
-	});
+router.get('/:id', (req, res) => {
+	const { id } = req.params;
+	console.log(req.params);
+	const { side } = req.query;
+	const text = cards[id][side];
+	const { hint } = cards[id];
+
+	const templateData = { id, text };
+	if (side === 'question') {
+		const td = templateData;
+		td.hint = hint;
+		td.displaySide = 'answer';
+		td.displaySideText = 'Answer';
+	} else if (side === 'answer') {
+		const td = templateData;
+		td.displaySide = 'question';
+		td.displaySideText = 'Question';
+	}
+
+	res.render('index', templateData);
 });
 
 module.exports = router;
